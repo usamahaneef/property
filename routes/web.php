60c435b538681b85c6e168e48671e1f0a4c7f92c
zohaib-workspace <zohaibworkspace@gmail.com>
2024-12-26 10:59:58 +0500

    rent page added

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return redirect()->route('admin.login');
 });
 
-// Route::get('/',[\App\Http\Controllers\Web\HomeController::class,'index'])->name('home');
+Route::get('/',[\App\Http\Controllers\Web\HomeController::class,'index'])->name('home');
 // Route::get('/become-partner',[\App\Http\Controllers\Web\HomeController::class,'becomePartner'])->name('web.partner');
 // Route::get('/privacy-policy',[\App\Http\Controllers\Web\HomeController::class,'privacyPolicy'])->name('web.privacy-policy');
 // Route::get('/account-deletion',[\App\Http\Controllers\Web\HomeController::class,'accountDeletion'])->name('web.account.deletion');
